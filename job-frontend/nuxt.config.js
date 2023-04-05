a0366739f90309dc67071053d0401c01ee6bff5e
gilank017 <gilank.zone@gmail.com>
2023-04-05 10:33:04 +0700

    add function get job

--- job-frontend/nuxt.config.js
+++ job-frontend/nuxt.config.js
@@ -51,7 +51,8 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    '@nuxtjs/proxy'
+    '@nuxtjs/proxy',
+    '@nuxtjs/moment'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
--- job-frontend/pages/jobs/index.vue
+++ job-frontend/pages/jobs/index.vue
@@ -1,4 +1,67 @@
 <template lang="pug">
-  .container.my-5
-    h5 ini halaman jobs
+  .job-list-page-wrapper.m-5.p-5
+    h5.mb-3 List Job
+    .text-right
+      button.btn.btn-success.btn-sm.text-white.waves-effect.waves-light.w-sm Buat Pekerjaan
+    .table-responsive.mt-3
+      table.table
+        thead
+          tr
+            th No
+            th Nama Pekerjaan
+            th Deskripsi
+            th Pendidikan Minimal
+            th Dibuat
+            th Berakhir
+            th Quota
+            th Aksi
+        tbody
+          tr(v-for="(val, index) in listJob" :key="index")
+            td {{ index + 1 }}
+            td {{ val.name }}
+            td {{ val.description }}
+            td {{ val.min_education }}
+            td {{ parseDate(val.created_date) }}
+            td {{ parseDate(val.expired_date) }}
+            td {{ val.quota }}
+            td
+              button.btn.btn-primary.mr-2 update
+              button.btn.btn-danger hapus
 </template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  data() {
+    return {
+      listJob: []
+    }
+  },
+  computed: {
+    ...mapState({
+      token: (state) => state.user.token
+    })
+  },
+  mounted() {
+    this.getListJob()
+  },
+  methods: {
+    async getListJob() {
+      const config = {
+        headers: {
+          'Authorization': `Bearer ` + this.token
+        }
+      }
+      const { status, data } = await this.$axios.get('job', config)
+      .then((res) => {
+        return res.data
+      })
+      if (status) {
+        this.listJob = data
+      }
+    },
+    parseDate(date) {
+      return this.$moment(date).format("DD-MM-YYYY")
+    }
+  }
+}
+</script>
